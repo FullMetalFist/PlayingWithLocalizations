976c6501b99d380775bd92f60661dcdefa4fedf9
Michael Vilabrera <m.vilabrera@gmail.com>
2015-06-21 17:44:46 -0400

    progress

--- PlayingWithInternationalization/PWIViewController.m
+++ PlayingWithInternationalization/PWIViewController.m
@@ -8,7 +8,12 @@
 
 #import "PWIViewController.h"
 
+NSString *const kBigLabelHorizontal = @"H:|-[_bigLabel]-|";
+NSString *const kBigLabelVertical = @"V:|-[_bigLabel]-|";
+
 @interface PWIViewController ()
+
+@property (nonatomic) UILabel *bigLabel;
 
 @end
 
@@ -17,6 +22,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self constructSubviews];
+    [self positionUI];
+    [self.view addConstraints:[self constraints]];
+}
+
+- (void)constructSubviews {
+    self.bigLabel = [[UILabel alloc] init];
+    self.bigLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.bigLabel setText:@"Big Label"];   // the old way
+    self.bigLabel.text = NSLocalizedString(@"Big Label", @"Grande la etiqueta");    // the new way
+    self.bigLabel.text = [NSString localizedStringWithFormat:NSLocalizedString(@"Big Label", @"Grande la etiqueta")];   // longer way
+    [self.view addSubview:self.bigLabel];
+}
+
+- (NSArray *)positionUI {
+    NSMutableArray *result = [NSMutableArray array];
+    
+    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(_bigLabel);
+    [result addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:kBigLabelHorizontal options:0 metrics:nil views:viewsDictionary]];
+    [result addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:kBigLabelVertical options:0 metrics:nil views:viewsDictionary]];
+    
+    return [NSArray arrayWithArray:result];
+}
+
+- (NSArray *)constraints{
+    NSMutableArray *result = [NSMutableArray array];
+    [result addObjectsFromArray:[self positionUI]];
+    
+    return [NSArray arrayWithArray:result];
 }
 
 - (void)didReceiveMemoryWarning {
